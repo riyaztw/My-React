8df151692fa53895a87c867e3faff78a00461200
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-01 12:32:13 +0530

    Add. adds the attributes passed in props to the elements

--- src/app.js
+++ src/app.js
@@ -1,41 +1,61 @@
-const Hello = function () {
-    return React.createElement("p", null, "Functional Component");
-}
-const helloWorld = React.createElement("h1", null, "Hello World")
-const HelloFunctionalComponent = React.createElement(Hello, null, null);
-const parent = React.createElement(
-    "div",
-    null,
-    helloWorld,
-    HelloFunctionalComponent,
-    "I'm a parent component"
-)
+// const Hello = function () {
+//     return React.createElement("p", null, "Functional Component");
+// }
+// const helloWorld = React.createElement("h1", null, "Hello World")
+// const HelloFunctionalComponent = React.createElement(Hello, null, null);
+// const parent = React.createElement(
+//     "div",
+//     null,
+//     helloWorld,
+//     HelloFunctionalComponent,
+//     "I'm a parent component"
+// )
 
-class ClassComponent {
-    render() {
-        return React.createElement("h3", null, "Class component");
-    }
-}
-const classComponent = React.createElement(ClassComponent, null, null);
+// class ClassComponent {
+//     render() {
+//         return React.createElement("h3", null, "Class component");
+//     }
+// }
+// const classComponent = React.createElement(ClassComponent, null, null);
 
-const FunctionalComponentWithProps = ({ name }) => {
-    return React.createElement("p", null, `Functional component with prop -${name}`);
-}
-const functionalComponentWithProp = React.createElement(FunctionalComponentWithProps, { name: "Riyaz" }, null)
+// const FunctionalComponentWithProps = ({ name }) => {
+//     return React.createElement("p", null, `Functional component with prop -${name}`);
+// }
+// const functionalComponentWithProp = React.createElement(FunctionalComponentWithProps, { name: "Riyaz" }, null)
 
-class ClassComponentWithProps extends React.Component {
+// class ClassComponentWithProps extends React.Component {
+//     constructor(props) {
+//         super(props)
+//     }
+//     render() {
+//         return React.createElement(
+//             "h3",
+//             null,
+//             `Class component with prop -${this.props.name}`
+//         )
+//     }
+
+// }
+
+// const classComponentWithProp = React.createElement(ClassComponentWithProps, { name: "Riyaz Pasha" }, null);
+class ClassComponentWithButtonActionPassedInProps extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
         return React.createElement(
-            "h3",
-            null,
-            `Functional component with prop -${this.props.name}`
+            "button",
+            { onclick: this.props.onClick },
+            `Class component with Buttom and action is passed in prop`
         )
     }
 
 }
+const classComponentWithButtonActionPassedInProp =
+    React.createElement(
+        ClassComponentWithButtonActionPassedInProps,
+        { onClick: () => alert("Button clicked") },
+        null
+    );
 
-const classComponentWithProp = React.createElement(ClassComponentWithProps, { name: "Riyaz Pasha" }, null);
-ReactDOM.render(classComponentWithProp, document.getElementById("root"))
+ReactDOM.render(classComponentWithButtonActionPassedInProp, document.getElementById("root"))
--- src/react.js
+++ src/react.js
@@ -6,7 +6,8 @@
         } else if (isFunction(element)) {
             return element(props);
         } else {
-            return handleHTMLelement(element, children);
+            console.log(props)
+            return handleHTMLelement(element, props, children);
         }
     }
 
@@ -15,7 +16,8 @@
         return component.render();
     }
 
-    function handleHTMLelement(element, children) {
+    function handleHTMLelement(element, props, children) {
+        console.log(element)
         const anElement = document.createElement(element);
         children.forEach(child => {
             if (typeof child === "object") {
@@ -25,10 +27,23 @@
                 anElement.innerHTML += child;
             }
         });
+        console.log(props)
+        Object.keys(props).forEach(propName => {
+            console.log(propName)
+            if (/^on.*$/.test(propName)) {
+                anElement.addEventListener(
+                    propName.substring(2).toLowerCase(),
+                    props[propName]
+                );
+            } else {
+                anElement.setAttribute(propName, props[propName]);
+            }
+        })
         return anElement;
     }
 
     function createElement(element, props, ...children) {
+        console.log(element, props)
         return anElement(element, props, children)
     }
 
